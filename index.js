40ab4649afb99af8483c1fd444ebdf90516e710b
Priyanka-243 <priyanka.kothawade24@gmail.com>
2021-05-08 00:06:59 +0530

    Find appointment

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 var nodemailer = require('nodemailer');
-const nashik_district_id = 389;
-const pune_district_id = 363;
+const nashik_district_id = "389";
+const pune_district_id = "363";
 const axios = require('axios');
-
+const to_pune = "vaccineupdateforrupesh@gmail.com;rupesh.kothawade33@gmail.com;tyagamayeebhadra@gmail.com";
+const to_nashik = "vaccineupdateforrupesh@gmail.com;rupesh.kothawade33@gmail.com";
 app.listen(process.env.PORT || 5000, () => {
     findAppointment();
 });
@@ -13,66 +14,76 @@ app.get('/', (req, res) => {
     res.send('Checking vaccine appointments...');
 });
 
-function sendAlert(appointment, centerName) {
-    let mailOptions = {
-        from: 'vaccineupdateforrupesh@gmail.com',
-        to: 'vaccineupdateforrupesh@gmail.com;rupesh.kothawade33@gmail.com;tyagamayeebhadra@gmail.com',
-        subject: appointment.available_capacity + ' Appointment(s) available in ' + centerName + ' on ' + appointment.date,
-        text: appointment
-    };
+function sendAlert(appointment, center, to) {
 
+    let mailOptions = {
+        from: "vaccineupdateforrupesh@gmail.com",
+        to: to,
+        subject: appointment.available_capacity + " Appointment(s) available in " + center.district_name + " " + center.name + " on " + appointment.date,
+        text: center.json
+    };
     transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
-            console.log('Error ' + error);
+            console.log("Error" + error);
         } else {
-            console.log('Email sent: ' + info.response)
+            console.log("Email sent: " + info.response);
         }
     });
+
 }
 
 function parseResponse(response) {
     if (response) {
-        let centers = response.centers;
+        let centers = response.data.centers;
         for (let i in centers) {
             let center = centers[i];
             let sessions = center.sessions;
-            for (let j in sessions) {
-                let appointment = sessions[j];
-                if (appointment.available_capacity > 0) {
-                    sendAlert(appointment, center.name);
+            if (Array.isArray(sessions)) {
+                for (let j in sessions) {
+                    let appointment = sessions[j];
+                    if (appointment.available_capacity > 0 && center.district_name === 'Nashik'
+                        && center.pincode.toString().startsWith("422")) {
+                        sendAlert(appointment, center, to_nashik);
+                    } else if (appointment.available_capacity > 0 && center.district_name === 'Pune'
+                        && center.pincode.toString().startsWith("411")) {
+                        sendAlert(appointment, center, to_pune);
+                    }
                 }
             }
+
         }
     }
 }
 
 const findAppointment = () => {
-    sendApiCall(pune_district_id).then(res => {
-        parseResponse(res);
-    });
-    sendApiCall(nashik_district_id).then(res => {
-        parseResponse(res);
-    });
+    sendApiCall(pune_district_id);
+    sendApiCall(nashik_district_id);
     setTimeout(findAppointment, 30000);
 }
 
-const sendApiCall = async (district_id) => {
-    var url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=' + district_id + '&date=' + new Date().toLocaleDateString();
-    console.log('Checking in ' + district_id + ' at ' + new Date().toLocaleTimeString());
-    axios.get(url)
+const sendApiCall = (district_id) => {
+    var url = 'http://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=' + district_id + '&date=' + new Date().toLocaleDateString();
+    let district_name = district_id === "363" ? "Pune" : "Nashik";
+    console.log('Checking in ' + district_name + ' at ' + new Date().toLocaleTimeString());
+    let config = {
+        headers: {
+            "user-agent": "Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
+        }
+    }
+    axios.get(url, config)
         .then(response => {
-            return response;
+            parseResponse(response);
         })
         .catch(error => {
-            console.log('Error ' + district_id + ' at ' + new Date().toLocaleTimeString() + ' -> ' + error.message);
+            console.log('Error ' + district_name + ' at ' + new Date().toLocaleTimeString() + ' -> ' + error.message);
         });
 }
 
 
 var transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
-        user: 'vaccineupdateforrupesh@gmail.com',
-        pass: 'Car@1234'
+        user: "vaccineupdateforrupesh@gmail.com",
+        pass: "Car@1234"
     }
 })
